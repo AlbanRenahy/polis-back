de1c90cfef24128a305a57e377a28dd0a628a3e9
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-09-27 09:41:55 +0200

    Initial commit with Spring

---
+++ src/main/java/com/example/polisback/PolisBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.polisback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolisBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolisBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/polisback/PolisBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.polisback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolisBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
