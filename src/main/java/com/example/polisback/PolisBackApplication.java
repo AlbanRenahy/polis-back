3920ee0a8262e71a20cd1ec7c106ef3451e08805
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-09-27 11:24:26 +0200

    applicaction.yaml modified

--- src/main/java/com/example/polisback/PolisBackApplication.java
+++ src/main/java/com/example/polisback/PolisBackApplication.java
@@ -1,10 +1,14 @@
 package com.example.polisback;
 
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Data
 @SpringBootApplication
 public class PolisBackApplication {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(PolisBackApplication.class, args);
