cff20fdd36091ffcdb6c291d79c5dd908eddc84f
Cosmocath <catherine.hermary@gmail.com>
2021-09-27 12:00:00 +0200

    creation du modele commentaire

--- src/main/java/com/example/polisback/models/Commentaire.java
+++ src/main/java/com/example/polisback/models/Commentaire.java
@@ -1,4 +1,25 @@
 package com.example.polisback.models;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "Commentaire")
+@Data
 public class Commentaire {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "contenu")
+    private String contenu;
+
+    @Column(name = "dateAjout")
+    private Date dateAjout;
+
+
+
+
 }
