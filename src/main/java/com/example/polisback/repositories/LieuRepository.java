fa97806fbc9fc01060fd4379fd41f375e9a7bd80
Cosmocath <catherine.hermary@gmail.com>
2021-10-15 16:44:45 +0200

    find lieu by id

--- src/main/java/com/example/polisback/repositories/LieuRepository.java
+++ src/main/java/com/example/polisback/repositories/LieuRepository.java
@@ -3,8 +3,12 @@ package com.example.polisback.repositories;
 import com.example.polisback.models.Lieu;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LieuRepository extends JpaRepository<Lieu, Long> {
     public Lieu findByNomLieu(String nomLieu);
 
     void deleteById(Long id);
+
+    public List<Lieu> findByCategorie(String nomCategorie);
 }
